fa68b66101b78ff6fb963911578abbcb39d4e934
juan.facorro <juan.facorro@gmail.com>
2011-04-01 17:57:49 +0000

    Conversion to symbol of tokenize.

--- LISP/fp-interpreter.lisp
+++ LISP/fp-interpreter.lisp
@@ -1,6 +1,11 @@
+;;----------------------------------------------
+;; fp-interpret
+;;----------------------------------------------
 (defun fp-interpret (code)
    code)
-
+;;----------------------------------------------
+;; tokenize
+;;----------------------------------------------
 (defun tokenize (str)
         (string-split str #\ ))
 ;----------------------------------------------
@@ -8,23 +13,23 @@
 ;----------------------------------------------
 (defun string-split (str delim)
   (let ((index (string-first-index str delim)))
-    (cond ((>= index 0) (append (list (subseq str 0 index)) (string-split (subseq str (+ index 1)) delim)))
+    (cond ((>= index 0) (append (list (intern (string-upcase (subseq str 0 index))))
+                                (string-split (subseq str (+ index 1)) delim)))
           (t 
-           (if (string= "" str) ''() (list str))))))
+           (if (string= "" str) '() (list (intern (string-upcase str))))))))
 ;----------------------------------------------
 ; string-first-index
 ;----------------------------------------------
-(defun string-first-index (str char)
-  (string-first-index-helper str char 0))
+(defun string-first-index (str c)
+  (string-first-index-helper str c 0))
 
-(defun string-first-index-helper (str char index)
+(defun string-first-index-helper (str c index)
   (cond ((string= str "") -1)
         (t
-         (if (char= char (char str 0))
+         (if (char= c (char str 0))
              index 
-             (string-first-index-helper (subseq str 1) char (+ 1 index))))))
+             (string-first-index-helper (subseq str 1) c (+ 1 index))))))
 ;----------------------------------------------
-
 (tokenize "Juan Martin")
 (string-split "id o 1" #\ )
 ;(map-symbol-to-function '/)
