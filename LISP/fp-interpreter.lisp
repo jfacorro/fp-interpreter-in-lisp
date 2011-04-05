1dd03a6d4b826139e3e93a6a2441c387338d84e7
juan.facorro <juan.facorro@gmail.com>
2011-04-05 03:55:19 +0000

    Defined hash with fp-functions.

--- LISP/fp-interpreter.lisp
+++ LISP/fp-interpreter.lisp
@@ -1,8 +1,62 @@
 ;;----------------------------------------------
+;; General hash table function
+;;----------------------------------------------
+(defun add-hash-item (hash-table key value)
+	(setf (gethash key hash-table) value))
+;;----------------------------------------------
+;; Define functions hash
+;;----------------------------------------------
+(defparameter *fp-functions* (make-hash-table))
+;;----------------------------------------------
+;; Add functions to hash-tables
+;;----------------------------------------------
+(defun fp-add-function (key value)
+	(add-hash-item *fp-functions* key value))
+;;----------------------------------------------
+;; Get function
+;;----------------------------------------------
+(defun fp-get-function (function-symbol)
+	(gethash function-symbol *fp-functions*))
+
+(fp-add-function 'id 		#'fp-id)
+(fp-add-function 'n 		#'fp-selector)
+(fp-add-function 'nr 		#'fp-selector-right)
+(fp-add-function 'tl 		#'fp-tl)
+(fp-add-function 'tlr 		#'fp-tlr)
+(fp-add-function 'atom 		#'fp-atom)
+(fp-add-function 'eq 		#'fp-eq)
+(fp-add-function 'null 		#'fp-null)
+(fp-add-function 'reverse	#'fp-reverse)
+(fp-add-function 'iota		#'fp-iota)
+(fp-add-function 'distl		#'fp-distl)
+(fp-add-function 'distr		#'fp-distr)
+(fp-add-function 'length	#'fp-length)
+(fp-add-function '-			#'fp--)
+(fp-add-function '+			#'fp-+)
+(fp-add-function '*			#'fp-*)
+(fp-add-function '%			#'fp-%)
+(fp-add-function '<			#'fp-<)
+(fp-add-function '>			#'fp->)
+(fp-add-function 'trans		#'fp-trans)
+(fp-add-function 'and		#'fp-and)
+(fp-add-function 'or		#'fp-or)
+(fp-add-function 'not		#'fp-not)
+(fp-add-function 'appendl	#'fp-appendl)
+(fp-add-function 'appendr	#'fp-appendr)
+(fp-add-function 'rot		#'fp-rot)
+(fp-add-function 'rotr		#'fp-rotr)
+(fp-add-function 'o			#'fp-compose)
+(fp-add-function '[]		#'fp-construct)
+(fp-add-function '~			#'fp-const)
+(fp-add-function 'cond		#'fp-cond)
+(fp-add-function '/			#'fp-insert)
+(fp-add-function 'alpha		#'fp-alpha)
+
+;;----------------------------------------------
 ;; fp-interpret
 ;;----------------------------------------------
 (defun fp-interpret (code)
-   code)
+   (mapcar #'fp-get-function (tokenize code)))
 ;;----------------------------------------------
 ;; tokenize
 ;;----------------------------------------------
@@ -32,5 +86,5 @@
 ;;----------------------------------------------
 (test #'tokenize "Juan Martin")
 (string-split "id o 1" #\ )
-;;(map-symbol-to-function '/)
-(fp-interpret '(id o 1))
+(fp-get-function '/)
+(fp-interpret "id o 1")
