76b1a1767acdfbd84e39be5ac51c6c52e00a95b5
juan.facorro <juan.facorro@gmail.com>
2011-04-13 04:31:10 +0000

    Improved string-explode implementation.

--- LISP/main.lisp
+++ LISP/main.lisp
@@ -2,7 +2,7 @@
 (defparameter *filepath* "C:\\JF\\My DropBox\\Facultad\\2011.1er.75.14.Lenguajes.Formales\\fp-interpreter-in-lisp\\LISP\\")
 ;----------------------------------------------------
 (defun compile-load (file-name &optional (v nil) (p nil))
-		(load (compile-file (concatenate 'string *filepath* file-name) :verbose v :print p)))
+		(load (compile-file (concatenate 'string *filepath* file-name) :verbose v :print p) :verbose v :print p))
 ;----------------------------------------------------
 (progn
 	(compile-load "packages.lisp")
@@ -15,13 +15,17 @@
 	(compile-load "fp-functions.lisp")
 	(compile-load "test-framework.lisp")
 	;; (load-code "tests.lisp")
-	(compile-load "fp-interpreter.lisp" nil t)
+	(compile-load "fp-interpreter.lisp" nil nil)
 	(compile-load "fp-evaluator.lisp")
-	(compile-load "fp-repl.lisp" nil t))
+	(compile-load "fp-repl.lisp" nil nil))
 
+(in-package :com.facorro.string)
+(string-explode "11.11.11.1.1111,111" "." ",")
+#|
 (in-package :com.facorro.debug)
 (debug-on)
 (in-package :com.facorro.fp.interpreter)
 (funcall (interpret "id") '1)
 (in-package :com.facorro.fp.repl)
 (fp-repl)
+|#
--- LISP/string.lisp
+++ LISP/string.lisp
@@ -14,31 +14,38 @@
 ;;----------------------------------------------
 (defun string-explode (str &rest delims)
 	"Explodes a given string using the specified delimiters"
-	(let (
-			(exploded-str 
-				(flatten 
-					(mapcar 
-						(lambda (s) (string-explode-helper s (first delims)))
-						(if (atom str) (list str) str)))))
-		(cond 
-			((null delims) str)
-			((= 1 (length delims)) exploded-str)
-			(t
-				(apply #'string-explode (append (list exploded-str) (rest delims)))))))
+	(let ((exploded-str (list str)))
+		; If there are delimiters then explode
+		(if (not (null delims))
+			; For each delimiter explode the string 
+			; and each instance afterwards
+			(dolist (delim delims)
+				(setf exploded-str (flatten (mapcar (explode-lambda delim) exploded-str)))))
+		exploded-str))
+;;----------------------------------------------
+;; explode-lambda
+;;----------------------------------------------
+(defun explode-lambda (delim)
+	"Generates a function with the delimiter"
+	(lambda (str) (string-explode-helper str delim)))
 ;;----------------------------------------------
 ;; string-explode-helper
 ;;----------------------------------------------
-(defun string-explode-helper (str delim)
-  (let ((index (search delim str))
-		(delim-length (length delim)))
-    (cond ((null index) (if (string= "" str) nil (list (string-upcase str))))
-		  ((> index 0) (append 
-							(list (string-upcase (subseq str 0 index))) 
-							(list delim)
-                            (string-explode-helper (subseq str (+ index delim-length)) delim)))
-		  ((= index 0) (append 
-							(list delim)
-                            (string-explode-helper (subseq str (+ index delim-length)) delim))))))
+(defun string-explode-helper (str delim &optional (explode t))
+	(let ((index (search delim str))
+		  (delim-length (length delim)))
+		(cond 
+			((null index) 
+				(if (empty-string? str) nil (list (string-upcase str))))
+			((> index 0)
+				(append
+					(list (string-upcase (subseq str 0 index)))
+					(list delim)
+                    (string-explode-helper (subseq str (+ index delim-length)) delim)))
+			((= index 0)
+				(cons
+					delim
+					(string-explode-helper (subseq str (+ index delim-length)) delim))))))
 ;;----------------------------------------------
 ;; Flatten
 ;;----------------------------------------------
@@ -48,3 +55,8 @@
 		((atom l) (list l))
 		(t (append 	(flatten (car l))
 					(flatten (cdr l))))))
+;;----------------------------------------------
+;; empty-string
+;;----------------------------------------------
+(defun empty-string? (str)
+	(string= "" str))
