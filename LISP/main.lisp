0ea47a1f1a0c616e9555a94c6d08df7624b58376
juan.facorro <juan.facorro@gmail.com>
2011-04-16 20:36:46 +0000

    Deleted all development code form main.lisp

--- LISP/main.lisp
+++ LISP/main.lisp
@@ -14,65 +14,8 @@
 	(compile-load "fp-functions-definition.lisp")
 	(compile-load "fp-functions.lisp")
 	(compile-load "test-framework.lisp")
-	;; (load-code "tests.lisp")
 	(compile-load "fp-interpreter.lisp" nil nil)
 	(compile-load "fp-evaluator.lisp")
 	(compile-load "fp-repl.lisp" nil nil))
 
-;-----------------------------------------
-(in-package :com.facorro.debug)
-;-----------------------------------------
-;(debug-on :com.facorro.parser)
-;-----------------------------------------
-(in-package :com.facorro.fp.parser)
-;-----------------------------------------
-;(defparameter rule (defrule "Replace strings for parser-friendly expressions"
-;		(string-replace arg
-;			; For 
-;			"[" "( construct ("
-;			"]" "))"
-;			"," ")(")))
-;(getf rule :function)
-;(funcall (getf rule :function) "[")
-;(parse "/appendr o(alpha(atom=>id;~<>))")
-;(parse "((/appendr) o (alpha(atom=>id;~<>)))")
-;(parse "id : <1 <2>>")
-;-----------------------------------------
-(in-package :com.facorro.fp.parser)
-;-----------------------------------------
-;(listify '("(" "/" "appendr" ")" "o" "(" "alpha" "(" "atom" "=>" "id" "~" "<>" ")" ")"))
-;(listify '("(" "1" "(" "2" "(" "4" ")" ")" "(" "5" ")" "3" ")"))
-;-----------------------------------------
-(in-package :com.facorro.string)
-;-----------------------------------------
-;(string-explode "11.11.11.1.1111,111" "." ",")
-;(string-split "11.11.11.1.1111,111" "." ",")
-;(string-replace "aaaabbbbcccc" "aaaa" "123456")
-;(string-replace "aaaabbbbcccc" "aa" "123456" "c" " ")
-;-----------------------------------------
-(in-package :com.facorro.fp.interpreter)
-;-----------------------------------------
-;(funcall (interpret "id") '1)
-;-----------------------------------------
-(in-package :com.facorro.fp.interpreter)
-;-----------------------------------------
-;(funcall (interpret "/ appendl") '(-1 1 1 1 (3 2)))
-;(funcall (interpret "[2°, 1°, id]") '(1 2))
-;(funcall (interpret "~fdgh") '((1 2) (3 4) (5 6)))
-;(funcall (interpret "((eq o [1°, 2°]) => ~1 ; ~2)") '(1 1))
-;(funcall (interpret "eq => ~T ; ~<>") '(1 2))
-;(funcall (interpret "2° o 2°") '(1 (2 3)))
-;(interpret "2° o 2° : <1, <2, 3>, 4>")
-;(interpret "2°: <1 <2 3> 4>")
-;(get-function "ID")
-(interpret "id : <1, 2, 3>")
-(interpret "1# : <1, 2, 3>")
-(interpret "1#r : <1, 2, 3>")
-(interpret "eq o [1#r, 1#] : <1 2 1>")
-;(interpret "def new : eq o [1°, 2°]")
-;(interpret "(new) : <1 1>")
-;-----------------------------------------
-(in-package :com.facorro.fp.repl)
-
-;-----------------------------------------
-;(fp-repl)
+(com.facorro.fp.repl:fp-repl)
